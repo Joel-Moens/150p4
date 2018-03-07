49383bf38afbf82ea707d9116087d98a9616ae96
Joel Moens <jmmoens@ucdavis.edu>
2018-03-06 17:17:11 -0800

    Added some comments, separated components to test malloc

--- libfs/fs.c
+++ libfs/fs.c
@@ -16,15 +16,14 @@ struct fsys {
 	struct fatblock ** fat;
 	struct rootblock * root;
 	struct datablock ** data;
-
-};
-struct filedescriptor {
+}; // File system struct, contains a superblock, list of fatblocks, rootblock, and list of datablocks
+struct __attribute__ ((__packed__)) filedescriptor {
 	char name[16];
 	uint64_t size;
 	uint32_t startindex;
 	char padding[10];
-};
-struct superblock {
+}; //File descriptor stores meta data for each file in root
+struct __attribute__ ((__packed__)) superblock  {
 	char sig[8]; // Must be "ECS150FS"
 	uint16_t blocksize;
 	uint16_t rootindex;
@@ -32,68 +31,74 @@ struct superblock {
 	uint16_t datablocknum;
 	uint8_t fatnum;
 	char padding[4079];
-};
+}; //Stores most needed info about disk
 struct __attribute__ ((__packed__)) fatblock {
-	// Keeps track of both the free data blocks and mapping between files 
-	// and the data blocks holding their content
 	uint16_t word[2048];
-};
+}; //Block that stores 2byte entries per datablock linking them if used in same file
+//THIS MAY NEED TO BE A uint16_t pointer that we initialize later?
 struct __attribute__ ((__packed__)) rootblock {
 	// Contains an entry for each file of fs
 	struct filedescriptor entry[128];
-};
+}; //Block that stores all 128 fd
 struct __attribute__ ((__packed__))  datablock {
 	void * memory;
+}; //block that stores file data
 
-};
-struct __attribute__ ((__packed__))  datalist {
-	void * memory;
 
-};
-struct __attribute__ ((__packed__))  fatlist {
-	void * memory;
-};
-
-struct fsys * fs;
+struct rootblock * root;
+struct superblock * super;
+struct fatblock ** fat;
+struct datablock ** data;
+//fs was going to be our filesystem that we use for this entire project, but trying to test out each component to see what the problem is
 /* TODO: Phase 1 */
-struct fsys* fs_malloc()
+int fs_malloc()
 {
-	struct fsys * temp = (void *) malloc(sizeof(void)*4);
-	temp->super = (struct superblock *) malloc(sizeof(struct superblock));
-	temp->fat = (struct fatblock **) malloc(sizeof(struct fatblock *) * temp->super->fatnum);
-	temp->root = (struct rootblock *) malloc(sizeof(struct rootblock));
-	temp->data = (struct datablock **) malloc(sizeof(struct datablock *) * temp->super->datablocknum);
+	// Allocate for each component
+	super = (struct superblock *) malloc(sizeof(struct superblock));
+	printf("super signature is: %s, super blocksize is: %d \n", super->sig, super->blocksize);
 	void * address = (void * ) malloc(sizeof(void));
 	block_read(0, address);
-	temp->super = (struct superblock *)address;
+	super = (struct superblock *)address;
 
-	printf("temp->super signature is: %s, temp->super blocksize is: %d \n", temp->super->sig, temp->super->blocksize);
-	if(strcmp(temp->super->sig, "ECS150FS") != 0)
+	// CURRENTLY STUCK HERE trying to allocated fatblock **
+	// I need a fatblock** because I'm making an array of fatblock *
+	// This next printf works, showing that block_read api and type casting works fine
+	printf("super signature is: %s, super blocksize is: %d \n", super->sig, super->blocksize);
+
+	//Tried googling this on stackoverflow and says to use valgrind, I don't know how to get that to test the code though
+	fat = (struct fatblock **) malloc((int)super->fatnum * sizeof(struct fatblock*) );
+	printf("super signature is: %s, super blocksize is: %d \n", super->sig, super->blocksize);
+	root = (struct rootblock *) malloc(sizeof(struct rootblock));
+	data = (struct datablock **) malloc(sizeof(struct datablock) * super->datablocknum);
+
+	printf("super signature is: %s, super blocksize is: %d \n", super->sig, super->blocksize);
+	char signature[8] = "ECS150FS";
+	if(strcmp(super->sig, signature) != 0)
 	{
 		printf("Error Signature does not equal ECS150FS \n");
 		//return NULL;
 	}
-	if((temp->super->fatnum + 2 + temp->super->datablocknum) != temp->super->blocksize)
+	if((super->fatnum + 2 + super->datablocknum) != super->blocksize)
 	{
 		printf("Error Block Size and Fatnum + Root + Super + Data Block Size are not equal. \n");
-		return NULL;
+		return -1;
 	}
 	uint16_t index = 1;
-	while(index < temp->super->rootindex)
+	while(index < super->rootindex)
 	{
 		block_read(index, address);
 		printf("Trying to insert fatblock %d\n", index-1);
-		temp->fat[index-1] = (struct fatblock *) address;
+		fat[index-1] = (struct fatblock *) address;
 		index++;	
 	}
 	block_read(index++, address);
-	temp->root = (struct rootblock *)address;
-	for(int i = 0; i < temp->super->datablocknum; i++)
+	root = (struct rootblock *)address;
+	for(int i = 0; i < super->datablocknum; i++)
 	{
 		block_read(index++, address);
-		temp->data[i] = (struct datablock *) address;
+		data[i] = (struct datablock *) address;
 	}
-	return temp;
+	return 0;
 }
 
 int fs_mount(const char *diskname)
@@ -109,8 +114,7 @@ int fs_mount(const char *diskname)
 		printf("TRYING TO MOUNT \n");
 		
 		//Make a superblock *
-		fs = fs_malloc();
-		if(fs == NULL)
+		if(fs_malloc() == -1)
 		{
 			return -1;
 		}
@@ -130,16 +134,16 @@ int fs_umount(void)
 int fs_getfreefat()
 {
 	int index = 0;
-	while(fs->fat[index] != 0)
+	while(fat[index] != 0)
 	{
 		index++;
 	}
-	return (fs->super->datablocknum - index);
+	return (super->datablocknum - index);
 }
 int fs_getfreefiles()
 {
 	int index = 0;
-	while(fs->root->entry[index].name[0] != '\0')
+	while(root->entry[index].name[0] != '\0')
 	{
 		index++;
 	}
@@ -147,14 +151,14 @@ int fs_getfreefiles()
 }
 int fs_info(void)
 {
-	printf("fs->super->sig is %s \n", fs->super->sig);
+	printf("super->sig is %s \n", super->sig);
 	printf("FS Info: \n total_blk_count=%d \n fat_blk_count=%d \n rdir_blk=%d \n data_blk=%d\n data_blk_count=%d\n fat_free_ratio=%d/%d\n rdir_free_ratio=%d/%d\n",
-		fs->super->blocksize,
-		fs->super->fatnum,
-		fs->super->rootindex,
-		fs->super->dataindex,
-		fs->super->datablocknum,
-		fs_getfreefat(),fs->super->datablocknum,
+		super->blocksize,
+		super->fatnum,
+		super->rootindex,
+		super->dataindex,
+		super->datablocknum,
+		fs_getfreefat(),super->datablocknum,
 		fs_getfreefiles(), 128);
 	return 0;	/* TODO: Phase 1 */
 }
